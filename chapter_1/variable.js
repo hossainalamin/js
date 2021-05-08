2261d40dc88c0edb85824064dfc33d42603cb90a
Md Alamin Hossain <hossainalamin980@gmail.com>
2021-05-08 17:06:58 +0600

    Variables & numbers

---
+++ chapter_1/variable.js
@@ -0,0 +1,3 @@
+var name = "Alamin"
+var age  = 23
+console.log(name + ' loves his country') 
---
+++ chapter_2/datatypes.js
@@ -0,0 +1,25 @@
+// Numbers
+var mathNumber = 11
+var floatNumber  = 10.1
+var hex = 0x16A
+var oct = 033
+var nn = Number('12')
+console.log(nn)
+console.log(hex)
+console.log(oct)
+var casting = Number.parseInt(floatNumber)
+console.log(casting)
+console.log(1/0)//infinity
+console.log("Abc" * 10)//NaN
+
+// string
+const text = "This is a text"
+//text = "This is another text" (error need var type)
+console.log(text)
+var str = String(123)
+console.log(str)
+
+// ****** Boolean ****
+var b1 = true
+var b2 = false
+console.log(b1)
