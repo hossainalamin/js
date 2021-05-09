8cc025c093970f859080d713b47489cb04af72f9
Md Alamin Hossain <hossainalamin980@gmail.com>
2021-05-09 15:58:11 +0600

    Operators,Math functions and Date

---
+++ chapter_2/operators.js
@@ -0,0 +1,22 @@
+//Operators
+var c ='50'
+var d = 50
+console.log(c === d)
+console.log(c !== d)
+
+//math functions 
+var a = 4.56
+console.log(Math.abs(a))
+console.log(Math.floor(a))
+console.log(Math.ceil(a))
+console.log(Math.round(a))
+
+//date
+var date = new Date()
+console.log(date.toDateString())
+console.log(date.toTimeString())
+console.log(date.toLocaleString())
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
