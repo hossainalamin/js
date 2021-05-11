08c98e3a7471a49c4000129a50331d8c2e5746ed
Md Alamin Hossain <hossainalamin980@gmail.com>
2021-05-11 15:51:53 +0600

    String

---
+++ chapter_4/string.js
@@ -0,0 +1,23 @@
+var n = 10
+var str = 10 + ''//it is a string
+console.log(str)
+
+var str1 = 'This is a \'string\''
+
+var a = '  aaaaaaZ   '
+console.log(a.trim())
+var b = 'aaaaaaz'//lowercaser is bigger than upoercase
+console.log(a > b)/false
+
+console.log('001' == 1)//true string auto convertion
+
+var x = 'I am'
+var y = 'Alamin'
+var z = x.concat(' ',y)
+console.log(z)
+
+var i = y.substr(3,4)
+console.log(i)
+console.log(a.startsWith(a))
+console.log(a.endsWith(z))
+console.log(z.split(' '))
