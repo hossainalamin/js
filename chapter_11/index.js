83ce78a537682de74c3187e96aebe0b7384400e1
Md Alamin Hossain <hossainalamin980@gmail.com>
2021-07-18 14:11:16 +0600

    THIS keyword example and Object litaral

---
+++ chapter_11/index.js
@@ -0,0 +1,21 @@
+var obj = {
+    length : 50,
+    width : 40,
+    draw : function(){
+        console.log("Im a rectecgle")
+    },
+    printrect : function(){
+        console.log("My length is "+this.length)
+        console.log("My width is "+this.width)
+    }
+}
+function myFunction(){
+    obj.printrect()
+}
+myFunction();
+var another = {
+    length : 100,
+    width : 50,
+    print : obj.printrect
+}
+another.print()
